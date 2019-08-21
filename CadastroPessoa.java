141f88c555879e2734ee7bb557580f8046e73819
gabriela thiesen <gabriela_thiesen@estudante.sc.senai.br>
2019-08-21 08:29:42 -0300

    a

---
+++ CadastroPessoa.java
@@ -0,0 +1,21 @@
+
+public class Main {
+	
+	
+	Endereco end2 = new Endereco (3,'rua do amor perfeito','pedra branca','palhoca');
+	Endereco end3 = new Endereco (4,'rua das camelias', 'pedra branca','palhoca');
+	Endereco end1 = new Endereco (1,'rua das gardenias','pedra branca','palhoca');
+	
+	Pessoa pes1 = new Pessoa(2, '15','gabriela','feminino');
+	Pessoa pes2 = new Pessoa (5, '24','matheus','masculino');
+	Pessoa pes3 = new Pessoa (6,'2','enzo','masculino');
+
+	public Main() {
+		super()
+		
+		
+
+		
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		
+
+		
+		Endereco end1 = new Endereco(1, "rua das gardenias", "pedra branca", "palhoca");
+		Endereco end2 = new Endereco (3,"rua do amor perfeito","pedra branca","palhoca");
+		Endereco end3 = new Endereco (4,"rua das camelias", "pedra branca","palhoca");
+		
+		
+		Pessoa pes1 = new Pessoa (2,"15","gabriela","feminino");
+		Pessoa pes2 = new Pessoa (5,"16","sophia","feminino");
+		Pessoa pes3 = new Pessoa (6,"15","giulia","feminino");
+
+		
+		
+
+	}
+
+}
